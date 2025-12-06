bc31eb666db4ea892bc20857d1540eda3fac621a
adel307 <adel.307.ahmed@gmail.com>
2025-12-06 11:21:57 +0200

    add MainApp to sittings

--- project_backend/CTR_API/settings.py
+++ project_backend/CTR_API/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Tickets'
+    'MainApp',
+    'Tickets',
 ]
 
 REST_FRAMEWORK = {}
