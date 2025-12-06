8afd76358748e51ac5e4e2ab15be37e11079dac5
adel307 <adel.307.ahmed@gmail.com>
2025-12-06 15:09:33 +0200

    fix error (add the new apps (Cinema, Screening, Seat) to instaled apps )

--- project_backend/CTR_API/settings.py
+++ project_backend/CTR_API/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'Cinema',
-    'Tickets',
     'Movie',
+    'Screening',
+    'Seat',
+    'Tickets',
 ]
 
 REST_FRAMEWORK = {}
