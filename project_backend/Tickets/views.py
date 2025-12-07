bab576ccbad8d513a5f3b77661ff68e3067c88bd
adel307 <adel.307.ahmed@gmail.com>
2025-12-07 16:53:50 +0200

    work on views

--- project_backend/Tickets/views.py
+++ project_backend/Tickets/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
 
 # Create your views here.
+
+#1 with out rest and no model quary FBV
+def NoRestNoModel(request):
+    guests = [
+        {
+            'name':'adel',
+            'phone':'011157',
+            'address':'adel-adress',
+        },
+        {
+            'name':'ali',
+            'phone':'011156',
+            'address':'ali-adress',
+        },
+        {
+            'name':'ahmed',
+            'phone':'011153',
+            'address':'ahmed-adress',
+        },
+    ]
+    return JsonResponse (guests,safe = False)
+
+
