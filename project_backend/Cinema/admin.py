54d8bffb52635f54aae62930359321a34f867aab
adel307 <adel.307.ahmed@gmail.com>
2025-12-09 09:45:22 +0200

    register models cinema , Screen

--- project_backend/Cinema/admin.py
+++ project_backend/Cinema/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Cinema, Screen
+from Seat.models import Seat
 
-# Register your models here.
+
+# --- Inline Admin for related models ---
+# This allows managing Screens directly from the Cinema page.
+class ScreenInline(admin.TabularInline):
+    model = Screen
+    extra = 1 # Number of empty forms to display
+
+class SeatInline(admin.TabularInline):
+    model = Seat
+    extra = 1
+
+# --- ModelAdmin Definitions ---
+
+@admin.register(Cinema)
+class CinemaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone_number')
+    search_fields = ('name', 'address')
+    # Add Screen management to the Cinema page
+    inlines = [ScreenInline] 
+
+@admin.register(Screen)
+class ScreenAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cinema', 'capacity')
+    list_filter = ('cinema',)
+    search_fields = ('name',)
+    # Add Seat management to the Screen page
+    inlines = [SeatInline]
