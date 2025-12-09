d2d4fed262dc8c622ac92e19ac85047453eac1f8
adel307 <adel.307.ahmed@gmail.com>
2025-12-09 09:58:12 +0200

    register model Screening

--- project_backend/Screening/admin.py
+++ project_backend/Screening/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Screening
 
-# Register your models here.
+@admin.register(Screening)
+class ScreeningAdmin(admin.ModelAdmin):
+    list_display = ('movie', 'screen', 'start_time', 'ticket_price')
+    list_filter = ('screen__cinema', 'movie', 'start_time')
+    search_fields = ('movie__title', 'screen__name')
+    # Use date/time hierarchy to easily navigate showtimes
+    date_hierarchy = 'start_time' 
